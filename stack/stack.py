b2045b90251e7bda36e5535d491d81f0c5d05eee
Ana Zoe <50473626+acarrillo3@users.noreply.github.com>
2020-05-11 20:12:30 -0400

    stacks file completo and test passing

--- stack/stack.py
+++ stack/stack.py
@@ -14,12 +14,18 @@ class Stack:
     def __init__(self):
         self.size = 0
         # self.storage = ?
+        self.storage = []
 
     def __len__(self):
-        pass
+        return self.size
 
     def push(self, value):
-        pass
+        self.storage.append(value)
+        self.size = self.size +1
+        return value
 
     def pop(self):
-        pass
+        if(len(self.storage)) == 0:
+            return None
+        self.size = self.size - 1
+        return self.storage.pop(len(self.storage) - 1)
