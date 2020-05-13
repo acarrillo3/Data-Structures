fae4713cd45dbc940a9f19722bc10ed0516ca2ea
Ana Zoe <50473626+acarrillo3@users.noreply.github.com>
2020-05-13 18:05:20 -0400

    test are passing for the first section

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -17,59 +17,59 @@ class BinarySearchTree:
 
     # Insert the given value into the tree
     def insert(self, value):
+        node = BinarySearchTree(value)
         if value < self.value:
-            if self.left:
+            if not self.left:
+                self.left = node
+            else:
                 self.left.insert(value)
+        else:
+            if not self.right:
+                self.right = node
             else:
-                self.left = BinarySearchTree(value)
-        elif value >= self.value:
-            if self.right:
                 self.right.insert(value)
-            else:
-                self.right = BinarySearchTree(value)
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
+        if target == self.value:
+            return True
         if target < self.value:
-            if self.left == None:
+            if not self.left:
                 return False
             return self.left.contains(target)
-        elif target > self.value:
-            if self.right == None:
+        else:
+            if not self.right:
                 return False
             return self.right.contains(target)
-        return True
+        
 
     # Return the maximum value found in the tree
     def get_max(self):
-        current_max = self
-        while current_max.right:
-            current_max = current_max.right
-        return current_max.value
+        current_max = self.value
+        if self.right:
+            current_max = self.right.get_max()
+        return current_max
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
+        fn(self.value)
         if self.left:
             self.left.for_each(fn)
         if self.right:
             self.right.for_each(fn)
-        return fn(self.value)
 
     # Part 2 -----------------------
 
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
     def in_order_print(self, node):
-        if self.left:
-            self.left.in_order_print(node)
-        print(self.value)
-        if self.right:
-            self.right.in_order_print(node)
+        pass
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
     def bft_print(self, node):
+        pass
 
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
@@ -78,11 +78,3 @@ class BinarySearchTree:
 
     # Stretch Goals -------------------------
     # Note: Research may be required
-
-    # Print Pre-order recursive DFT
-    def pre_order_dft(self, node):
-        pass
-
-    # Print Post-order recursive DFT
-    def post_order_dft(self, node):
-        pass
